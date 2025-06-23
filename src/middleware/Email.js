83f0f4bab85dcab6d721b7847840652e53e4c3ff
SHREYANSH SINGH <sshreyansh962@gmail.com>
2025-06-24 01:27:03 +0530

    app template for token in email1

--- src/middleware/Email.js
+++ src/middleware/Email.js
@@ -1,3 +1,20 @@
+import { transporter } from "../middleware/EmailConfig.js"
+
+export const sendEmail=async(email,verificationToken)=>{
+    try {
+    const info = await transporter.sendMail({
+        from: '"Radon" radon4125@gmail.com',
+        to: email,
+        subject: "Verify Your Email",
+        text: "Verify Your Email", 
+        html:verificationToken,
+    });
+    console.log("Email for verification send successfully!");
+    
+    } catch (error) {
+        console.log(error);
+    }
+}
 export const sendEmail = async (email, verificationToken) => {
   try {
     const htmlContent = `
@@ -16,7 +33,7 @@ export const sendEmail = async (email, verificationToken) => {
     `;
 
     const info = await transporter.sendMail({
-      from: '"Radon" <radon4125@gmail.com>',
+      from: '"Radon" radon4125@gmail.com',
       to: email,
       subject: "Verify Your Email",
       text: `Your verification code is: ${verificationToken}`,
@@ -28,3 +45,4 @@ export const sendEmail = async (email, verificationToken) => {
     console.log(error);
   }
 }
+
