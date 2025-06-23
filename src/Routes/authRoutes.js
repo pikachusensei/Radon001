487d8914d2558827031d2307d1c1e7643b835606
SHREYANSH SINGH <sshreyansh962@gmail.com>
2025-06-24 00:40:15 +0530

    app resend token done bug solved

--- src/Routes/authRoutes.js
+++ src/Routes/authRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { registerUser,verifyEmail } from "../controllers/authController.js";
+import { registerUser,verifyEmail,resendCode } from "../controllers/authController.js";
 import { loginUser } from "../controllers/authController.js";
 
 
