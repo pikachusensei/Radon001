13a6a9559fa57c56fde3300735bbc821a631c40c
SHREYANSH SINGH <sshreyansh962@gmail.com>
2025-06-28 16:07:41 +0530

    app nodemailer env setup done

--- src/middleware/EmailConfig.js
+++ src/middleware/EmailConfig.js
@@ -1,12 +1,15 @@
 import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
+  port: parseInt(process.env.Email_Port, 10),
   secure: false, // true for 465, false for other ports
   auth: {
     user: "radon4125@gmail.com",
-    pass: "aggk cszy kesb gzxc",
+    pass: process.env.PASS,
   },
 });
 
